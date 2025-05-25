5017d159b388df524b6ce92decca43ff921de47e
Eli <elijames2301@gmail.com>
2025-05-25 15:46:36 -0400

    Update resume - fix for drag drop use file explorer

--- .eleventy.js
+++ .eleventy.js
@@ -6,13 +6,14 @@ module.exports = function(eleventyConfig) {
     posts.forEach(post => {
       console.log(`- ${post.inputPath} (${post.date.toISOString()})`);
     });
+    eleventyConfig.addPassthroughCopy("website/eliresume.pdf");
     return posts;
   });
 
   // Copy any static assets
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("website/eliresumemay2025.pdf");
+  eleventyConfig.addPassthroughCopy("website/eliresume.pdf");
 
 
   return {
